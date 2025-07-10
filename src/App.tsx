09d12e32baecd58249229f9fc25dfbf3f84113d7
Robert Kirsz <robert.kirsz@gmail.com>
2025-07-10 18:27:28 +0200

    Add global types

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
 export default function App() {
+  const game: Game = {
+    id: '1',
+    createdAt: new Date(),
+    players: ['1'],
+  }
+  
+  const user: User = {
+    id: '1',
+    name: 'John Doe',
+    createdAt: new Date(),
+    decks: [],
+  }
+  
   return <div className="text-3xl font-bold underline">Hello World</div>
 }
---
+++ src/types/global.d.ts
@@ -0,0 +1,22 @@
+type Decks = {
+  id: string;
+  name: string;
+  createdAt: Date;
+  commanders?: string[];
+  // add more fields as needed
+};
+
+type User = {
+  id: string;
+  name: string;
+  createdAt: Date;
+  decks: Decks['id'][];
+  // add more fields as needed
+}; 
+
+type Game = {
+  id: string;
+  createdAt: Date;
+  players: User['id'][];
+  // add more fields as needed
+};
