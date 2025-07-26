deb2457c6ac6fe57886992c32471129647f9ab5c
Robert Kirsz <robert.kirsz@gmail.com>
2025-07-26 13:23:04 +0200

    Add @vitejs/plugin-basic-ssl dependency, and configure Vite to use SSL for local development

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import tailwindcss from '@tailwindcss/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()]
+  plugins: [
+    react(),
+    tailwindcss(),
+    basicSsl({
+      name: 'magic-counter-2',
+      domains: ['localhost', '127.0.0.1']
+    })
+  ],
+  server: {
+    host: true
+  }
 })
